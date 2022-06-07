aed0f6fc2f0bf9a37e2443b35a7b45f06e46bdad
Quentin Pongratz <qpongratz@gmail.com>
2022-06-07 14:40:52 -0500

    Setup screenshot slideshow

---
+++ script.js
@@ -0,0 +1,43 @@
+function mod(n,m) {
+  return ((n % m) +m) % m;
+}
+
+const slideManager = (id) => {
+  const slideshow = document.getElementById(id);
+  const leftButton = slideshow.querySelector('#left');
+  const rightButton = slideshow.querySelector('#right');
+  const slides = slideshow.querySelectorAll('.screenshot');
+  let currentScreenshot = 0;
+
+  const hideAndShow = () => {
+    slides.forEach ((element, index) => {
+      if(index == currentScreenshot) {
+        element.classList.remove('hidden');
+      } else {
+        element.classList.add('hidden');
+      };
+    });
+  };
+
+  const nextScreenshot = () => {
+    currentScreenshot = mod((currentScreenshot + 1), slides.length);
+    hideAndShow();
+  }
+  const previousScreenshot = () => {
+    currentScreenshot = mod((currentScreenshot - 1), slides.length);
+    hideAndShow();
+  }
+
+  leftButton.addEventListener('click', previousScreenshot);
+  rightButton.addEventListener('click', nextScreenshot);
+  hideAndShow();
+
+  return {
+
+  }
+}
+const screenshots = document.querySelectorAll('.screenshots')
+
+screenshots.forEach ((element) => {
+  slideManager(element.id);
+});
